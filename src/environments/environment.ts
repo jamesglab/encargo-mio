4e48215f33b83098af41e64cb808a184a3456526
Miguel torres <lt@wap.us>
2021-09-22 15:16:21 -0500

    remove ngrok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   url_api: '',
   microservices: {
     user: "https://dev-api.encargomio.com/api/v1/user/",
-    management: "https://e9fb-190-217-51-38.ngrok.io/api/v1/management/",
+    management: "https://dev-api.encargomio.com/api/v1/management/",
     orders: "https://dev-api.encargomio.com/api/v1/orders/"
   },
   firebaseConfig: {
