920b0181ff0f18127b03d7667c19c05c82640932
Hector Garzon <hectorj.24@hotmail.com>
2021-12-15 14:52:53 -0500

    remove console.log

--- src/app/shared/widget/transaction/modals/modal-locker-entry/modal-locker-entry.component.ts
+++ src/app/shared/widget/transaction/modals/modal-locker-entry/modal-locker-entry.component.ts
@@ -213,7 +213,6 @@ export class ModalLockerEntryComponent implements OnInit {
   }
 
   cleanData() {
-    // console.log("ENTRO AL CLEAN DATA");
     this.allGuides = [];
     this.toHome = { status: false, to_home: false };
     this.lockerForm.controls.product_description.setValue(null);
