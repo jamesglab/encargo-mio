d683ad6d12e1a59d20f4ee58af5492f447c4b924
Hector Garzon <hectorj.24@hotmail.com>
2022-02-23 17:21:25 -0500

    fixes

--- src/app/pages/lockers/components/lockers-table/lockers-table.component.ts
+++ src/app/pages/lockers/components/lockers-table/lockers-table.component.ts
@@ -40,6 +40,8 @@ export class LockersTableComponent implements OnInit {
   public filteredUsers: Observable<string[]>;
   public counts: any;
 
+  public isIphone: boolean = false;
+
   //SERVICIOS Y DEMAS IMPORTACIONES
   constructor(
     public modalService: NgbModal,
@@ -51,11 +53,32 @@ export class LockersTableComponent implements OnInit {
   ngOnInit(): void {
     this.getUsers();
     this.getAllLockers();
+    this.checkOperativeSystem();
+  }
+
+  checkOperativeSystem() {
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+      if (document.cookie.indexOf("iphone_redirect=false") == -1) {
+        this.isIphone = true;
+      } else {
+        this.isIphone = false;
+      }
+    }
   }
 
   ngOnChanges() {
     if (this.refreshTableStatus) {
       this.getAllLockers();
+    }
+  }
+
+  keyDownFunction(event: any) {
+    if (this.isIphone) {
+      if (event.keyCode === 13) { // Si presiona el botón de intro o return en safari en IOS.
+        this.getAllLockers();
+      }
+    } else {
+      return;
     }
   }
 
--- src/app/pages/products/search-product/search-product.component.ts
+++ src/app/pages/products/search-product/search-product.component.ts
@@ -8,6 +8,7 @@ import { GET_STATUS, validateShippingstatus } from 'src/app/_helpers/tools/utils
   templateUrl: './search-product.component.html',
   styleUrls: ['./search-product.component.scss']
 })
+
 export class SearchProductComponent implements OnInit {
 
   public product_id: string = null;
@@ -15,10 +16,22 @@ export class SearchProductComponent implements OnInit {
   private unsuscribe: Subscription[] = [];
 
   public isLoading: boolean = false;
+  public isIphone: boolean = false;
 
   constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.checkOperativeSystem();
+  }
+
+  checkOperativeSystem() {
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+      if (document.cookie.indexOf("iphone_redirect=false") == -1) {
+        this.isIphone = true;
+      } else {
+        this.isIphone = false;
+      }
+    }
   }
 
   onSearch($event: any) {
@@ -27,12 +40,26 @@ export class SearchProductComponent implements OnInit {
       .subscribe((res: any) => {
         const { product_info } = res;
         this.product_info = product_info;
-        this.isLoading = false; 
-      }, (err) => { this.isLoading = false; throw err; });
+        this.isLoading = false;
+      }, err => {
+        this.isLoading = false;
+        throw err;
+      });
     this.unsuscribe.push(infoSubscr);
   }
 
-  onImageError(event) { event.target.src = "https://i.imgur.com/riKFnErh.jpg"; }
+  keyDownFunction(event: any, value?: any) {
+    console.log(value);
+    if (this.isIphone) {
+      if (event.keyCode === 13) { // Si presiona el botón de intro o return en safari en IOS.
+        // this.onSearch();
+      }
+    } else {
+      return;
+    }
+  }
+
+  onImageError(event: any) { event.target.src = "https://i.imgur.com/riKFnErh.jpg"; }
 
   quotationStatus(status: string): string {
     return GET_STATUS(status)
