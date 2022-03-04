d6b8e3513f6854969ab5e4c60205a7ceb717886c
rolo <hectorj.24@hotmail.com>
2022-03-04 17:33:00 -0500

    Fix order_service in create order parse tool

--- src/app/_helpers/tools/create-order-parse.tool.ts
+++ src/app/_helpers/tools/create-order-parse.tool.ts
@@ -105,7 +105,7 @@ export const insertOnlyLocker = (form: any, order_service: string, income?: any)
     let data = {
         "locker": form.user.locker_id,
         "guide_number": form.guide_number ? form.guide_number : null,
-        "order_service": order_service ? order_service : (form.order_service?.id ? form.order_service.id : form.order_service),
+        "order_service": order_service ? order_service : (form.order_service ? form.order_service.order_service.id : form.order_service),
         "id": income ? parseInt(income) : null,
         "conveyor": form.conveyor.id,
         "products": form.products,
