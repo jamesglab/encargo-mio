879d3ef2a848105b2619da454b889c0a125b19b6
rolo <hectorj.24@hotmail.com>
2022-03-06 09:19:51 -0500

    remove perrmission guard

--- src/app/core/guards/permissions.guard.ts
+++ src/app/core/guards/permissions.guard.ts
@@ -17,24 +17,24 @@ export class PermissionsGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
-    const permissions = this.authenticationService.getItem('permissions');
-    let access_module = false;
+    // const permissions = this.authenticationService.getItem('permissions');
+    // let access_module = false;
 
-    MENU.map(m => {
-      m.subItems.map(sub => {
-        if (sub.link == state.url && permissions[sub.code]) {
-          access_module = true;
-        }
-      });
-    });
+    // MENU.map(m => {
+    //   m.subItems.map(sub => {
+    //     if (sub.link == state.url && permissions[sub.code]) {
+    //       access_module = true;
+    //     }
+    //   });
+    // });
 
-    if (access_module || state.url.split("/")[2] == "addressess" || state.url.split("/")[2].includes('?')) { // El parámetro de ? es porque cuando se envien ids a través de query params no lo devuelva a landing
-      return true; //ESTO ES POR QUE ADDRESSESS NO NECESITA SER RENDERIZADO EN LA SIDE BAR
-    }
-    Swal.fire('Acceso Denegado', 'No dispones de los permiso para acceder a este módulo. Contacta con el administrador.', 'info').then(r => {
-      this.router.navigate(['/landing']);
-      return false;
-    });
+    // if (access_module || state.url.split("/")[2] == "addressess" || state.url.split("/")[2].includes('?')) { // El parámetro de ? es porque cuando se envien ids a través de query params no lo devuelva a landing
+    //   return true; //ESTO ES POR QUE ADDRESSESS NO NECESITA SER RENDERIZADO EN LA SIDE BAR
+    // }
+    // Swal.fire('Acceso Denegado', 'No dispones de los permiso para acceder a este módulo. Contacta con el administrador.', 'info').then(r => {
+    //   this.router.navigate(['/landing']);
+    //   return false;
+    // });
     return true;
   }
 
